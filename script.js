94719a6fd21fad2b700ab80a704d8cacfbc0ea3a
RJ <rjjor23@gmail.com>
2021-12-22 21:17:44 -0800

    sets up the stucture of the page

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log("Hello");
+
+const container = document.getElementById("gridContainer");
+
+function makeGrid(rows, cols) {
+  container.style.setProperty('--grid-rows', rows);
+  container.style.setProperty('--grid-cols', cols);
+
+  for(c = 0; c < (rows*cols); c++) {
+    let cell = document.createElement("div");
+    // cell.innerText = (c + 1);
+
+    container.appendChild(cell).className = 'gridItem';
+  };
+};
+
+makeGrid(4,4);
