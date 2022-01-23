c7101b6d4806c0779a49652b9c907e938469683b
RJ <rjjor23@gmail.com>
2022-01-22 21:50:52 -0800

    builds website

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 console.log("Hello");
 
+// global variables
 const container = document.getElementById("gridContainer");
 
+// console log test variables
+
+console.log(container);
+
+// functions 
+
+// creates grid function
 function makeGrid(rows, cols) {
   container.style.setProperty('--grid-rows', rows);
   container.style.setProperty('--grid-cols', cols);
@@ -14,4 +22,20 @@ function makeGrid(rows, cols) {
   };
 };
 
-makeGrid(4,4);
+makeGrid(64,64);
+
+// change color on hover function
+let test = document.querySelectorAll("#gridContainer > div");
+console.log(test);
+
+// function
+test.forEach((item) => {
+  item.count = 0;
+  item.addEventListener("mouseenter", (e) => {
+    e.target.style.backgroundColor = '#353535';
+  });
+});
+
+// range slider
+let range = document.querySelector('#gridRange');
+console.log(range);
